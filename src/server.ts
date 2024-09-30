f610575f2ddb0ff18ad3e3c6c9c46e5287260092
Mauricio Daniel Garcia <magadi111@gmail.com>
2024-09-30 17:15:01 -0300

    add handle errors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { API_PORT } from '@/config/index';
 import { departmentRouter } from './department/adapters/routes';
 import { employeeRouter } from './employee/adapters/routes';
@@ -9,6 +9,16 @@ app.use(express.json());
 
 app.use('/departments', departmentRouter);
 app.use('/employee', employeeRouter);
+
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  console.error(
+    '[ERROR]',
+    err instanceof Error
+      ? err.stack
+      : JSON.stringify(err.message) || JSON.stringify(err)
+  );
+  res.status(500).json({ error: 'Something wrong happened' });
+});
 
 export async function initServer() {
   return new Promise<void>((resolve, reject) => {
